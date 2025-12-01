fb1a9fc840a8b834c082c56c5dbabbce8d5eec6c
Your Name <zena252010@gmail.com>
2025-12-01 12:43:24 +0600

    Подгрузка каталога на главную страницу

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,106 +1,93 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 import '../style/Catalog.css'
 
 
 //Здесь должна быть логика подгрузки списка часов из БД и провильная вставка их в таблицу
 function Catalog(){
 
+  const [watches, setWatches] = useState([])
+  
+  const fetchWatches = async () => {
+    try {
+      const response = await axios.get('http://localhost:5000/api/watches');
+      setWatches(response.data);
+    } catch (e) {
+      console.error('Ошибка при загрузке часов:', e);
+    }
+  };
+  
+
+fetchWatches();
+  
+  
   return (
     <div>Catalog
-
       <h1>Каталог часов</h1>
-  <br/>
+    <br/>
   
-  <div class="block_filters">
-    <div class="filters">
-      <a href="#" class="filter" id="prise" onclick="alert_stop()">ЦЕНА</a>
-      <a href="#" class="filter" id="mark">МАРКА</a>
-      <a href="#" class="filter" id="pol">ПОЛ</a>
-      <a href="#" class="filter" id="mechanism">МЕХАНИЗМ</a>
-      <a href="#" class="filter" id="other">ДРУГИЕ ФИЛЬТРЫ</a>
-      <a href="#" class="filter" id="new">НОВИНКА</a>
-      <a href="#" class="filter" id="sale">СКИДКА</a>
-      <a href="#" class="filter" id="stock">В НАЛИЧИИ</a>
-      <a href="#" class="filter" id="stock_in_shop">В НАЛИЧИИ В МАГАЗИНАХ</a>
+  <div className="block_filters">
+    <div className="filters">
+      <a href="#" className="filter" id="prise">ЦЕНА</a>
+      <a href="#" className="filter" id="mark">МАРКА</a>
+      <a href="#" className="filter" id="pol">ПОЛ</a>
+      <a href="#" className="filter" id="mechanism">МЕХАНИЗМ</a>
+      <a href="#" className="filter" id="other">ДРУГИЕ ФИЛЬТРЫ</a>
+      <a href="#" className="filter" id="new">НОВИНКА</a>
+      <a href="#" className="filter" id="sale">СКИДКА</a>
+      <a href="#" className="filter" id="stock">В НАЛИЧИИ</a>
+      <a href="#" className="filter" id="stock_in_shop">В НАЛИЧИИ В МАГАЗИНАХ</a>
     </div>
-    <div class="line"></div>
-    <div class="catalog-grid" id="">
-      <div class="catalog-item">
-        <a href="item.html" class="item-click">
-          <img src="/img/item1.png" alt="" srcset="" class="img_item"/>
-          <label class="item-name">ЧАСЫ СЛАВА ИНСТИНКТ 6239485/2025</label>
-          <label for="" class="prise">5 863р</label>
+    <div className="line"></div>
+
+    <div className="catalog-grid" id="">
+
+      {watches.map(watch => (
+        <div className="catalog-item">
+          <a href="item.html" className="item-click">
+              {watch.imgUrl ? (
+                        <img src={watch.imgUrl} alt={watch.name} className="img_item" />
+                      ) : ( <img src="" className="img_item" /> )}            
+            <label className="item-name">{watch.name} {watch.brand}</label>
+            <label for="" className="prise">watch.prise</label>
+          </a>  
+            <input type="button" value="Купить" className="button-pay" id="b-pay"/>
+          <a href="#" className="compaer">Добавить к сравнению</a>
+        </div>
+        
+      ))}
+
+
+
+      <div className="catalog-item">
+        <a href="item.html" className="item-click">
+          <img src="/img/item1.png" alt="" srcset="" className="img_item"/>
+          <label className="item-name">ЧАСЫ СЛАВА ИНСТИНКТ 6239485/2025</label>
+          <label for="" className="prise">5 863р</label>
         </a>  
-          <input type="button" value="Купить" class="button-pay" id="b-pay" onclick="alert_stop()"/>
-        <a href="#" class="compaer">Добавить к сравнению</a>
+          <input type="button" value="Купить" className="button-pay" id="b-pay" onclick="alert_stop()"/>
+        <a href="#" className="compaer">Добавить к сравнению</a>
       </div>
-      <div class="catalog-item">
-        <a href="item.html" class="item-click">
-          <img src="/img/item1.png" alt="" srcset="" class="img_item"/>
-          <label class="item-name">ЧАСЫ СЛАВА ИНСТИНКТ 6239485/2025</label>
-          <label for="" class="prise">5 863р</label>
+      <div className="catalog-item">
+        <a href="item.html" className="item-click">
+          <img src="/img/item1.png" alt="" srcset="" className="img_item"/>
+          <label className="item-name">ЧАСЫ СЛАВА ИНСТИНКТ 6239485/2025</label>
+          <label for="" className="prise">5 863р</label>
         </a>  
-          <input type="button" value="Купить" class="button-pay" id="b-pay" onclick="alert_stop()"/>
-        <a href="#" class="compaer">Добавить к сравнению</a>
+          <input type="button" value="Купить" className="button-pay" id="b-pay" onclick="alert_stop()"/>
+        <a href="#" className="compaer">Добавить к сравнению</a>
       </div>
-      <div class="catalog-item">
-        <a href="item.html" class="item-click">
-          <img src="/img/item1.png" alt="" srcset="" class="img_item"/>
-          <label class="item-name">ЧАСЫ СЛАВА ИНСТИНКТ 6239485/2025</label>
-          <label for="" class="prise">5 863р</label>
+      <div className="catalog-item">
+        <a href="item.html" className="item-click">
+          <img src="/img/item1.png" alt="" srcset="" className="img_item"/>
+          <label className="item-name">ЧАСЫ СЛАВА ИНСТИНКТ 6239485/2025</label>
+          <label for="" className="prise">5 863р</label>
         </a>  
-          <input type="button" value="Купить" class="button-pay" id="b-pay" onclick="alert_stop()"/>
-        <a href="#" class="compaer">Добавить к сравнению</a>
-      </div>
-      <div class="catalog-item">
-        <a href="item.html" class="item-click">
-          <img src="/img/item1.png" alt="" srcset="" class="img_item"/>
-          <label class="item-name">ЧАСЫ СЛАВА ИНСТИНКТ 6239485/2025</label>
-          <label for="" class="prise">5 863р</label>
-        </a>  
-          <input type="button" value="Купить" class="button-pay" id="b-pay" onclick="alert_stop()"/>
-        <a href="#" class="compaer">Добавить к сравнению</a>
+          <input type="button" value="Купить" className="button-pay" id="b-pay" onclick="alert_stop()"/>
+        <a href="#" className="compaer">Добавить к сравнению</a>
       </div>
     </div>
   </div>
-
-
-
-
-
-
-
-
-
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
-      <br/>
 
 
     </div>
